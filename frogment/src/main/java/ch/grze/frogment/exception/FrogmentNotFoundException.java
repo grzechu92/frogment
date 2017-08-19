0948c53756da3e6504b2733066cbd452559142af
Grzechu <mail@grze.ch>
2017-08-19 22:40:59 +0200

    removed unused exception

--- frogment/src/main/java/ch/grze/frogment/exception/FrogmentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ch.grze.frogment.exception;
-
-public class FrogmentNotFoundException extends RuntimeException {
-    public FrogmentNotFoundException(String canonicalName) {
-        super(canonicalName);
-    }
-}
