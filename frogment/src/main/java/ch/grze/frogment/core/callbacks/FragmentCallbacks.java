1dc3d9eb7e1f2d15ea62df4b33e51fed2363e3b8
Grzechu <mail@grze.ch>
2017-10-06 21:05:02 +0200

    fragment lifecycle callbacks logic improved

--- frogment/src/main/java/ch/grze/frogment/core/callbacks/FragmentCallbacks.java
+++ frogment/src/main/java/ch/grze/frogment/core/callbacks/FragmentCallbacks.java
@@ -1,29 +1,25 @@
 package ch.grze.frogment.core.callbacks;
 
-
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentManager.FragmentLifecycleCallbacks;
 
 import ch.grze.frogment.core.Core;
 import ch.grze.frogment.core.CoreAware;
 
-public class FragmentCallbacks extends FragmentLifecycleCallbacks {
-    private final Core core;
-
+public class FragmentCallbacks extends AbstractFragmentLifecycleCallbacks {
     public FragmentCallbacks(Core core) {
-        this.core = core;
+        super(core);
     }
 
     @Override
     public void onFragmentAttached(FragmentManager fm, Fragment f, Context context) {
         super.onFragmentAttached(fm, f, context);
 
-        if (f instanceof CoreAware) {
-            final CoreAware coreAware = (CoreAware) f;
+        try {
+            final CoreAware coreAware = getTypedFragment(f);
 
             coreAware.setCore(core);
-        }
+        } catch (ClassCastException ignored) {}
     }
 }
