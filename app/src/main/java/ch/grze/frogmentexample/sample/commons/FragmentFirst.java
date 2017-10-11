14b4316ecef0240943e7fac99a12a4254efcef25
Grzechu <mail@grze.ch>
2017-10-11 20:41:45 +0200

    code cleanup

--- app/src/main/java/ch/grze/frogmentexample/sample/commons/FragmentFirst.java
+++ app/src/main/java/ch/grze/frogmentexample/sample/commons/FragmentFirst.java
@@ -1,28 +1,15 @@
 package ch.grze.frogmentexample.sample.commons;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ch.grze.frogment.frogment.FrogmentComponent;
+import ch.grze.frogment.frogment.AbstractFrogment;
 import ch.grze.frogment.frogment.FrogmentInterface;
 import ch.grze.frogmentexample.R;
 
-public class FragmentFirst extends Fragment implements FrogmentInterface {
-    private FrogmentComponent component;
-
-    @Override
-    public FrogmentComponent getFrogmentComponent() {
-        return component;
-    }
-
-    @Override
-    public void setFrogmentComponent(FrogmentComponent component) {
-        this.component = component;
-    }
-
+public class FragmentFirst extends AbstractFrogment implements FrogmentInterface {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_first, container, false);
--- frogment/src/main/java/ch/grze/frogment/frogment/FragmentCallbacks.java
+++ frogment/src/main/java/ch/grze/frogment/frogment/FragmentCallbacks.java
@@ -1,6 +1,5 @@
 package ch.grze.frogment.frogment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,12 +11,6 @@ public class FragmentCallbacks extends AbstractFragmentLifecycleCallbacks {
     public FragmentCallbacks(Core core) {
         super(core);
     }
-
-    @Override
-    public void onFragmentAttached(FragmentManager fm, Fragment f, Context context) {
-        super.onFragmentAttached(fm, f, context);
-    }
-
 
     @Override
     public void onFragmentPreCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
