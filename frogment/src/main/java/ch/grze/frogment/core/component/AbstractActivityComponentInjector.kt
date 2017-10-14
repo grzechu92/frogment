ea20e5429c4dc705e3e1a7bb93316d4fbffd9891
Grzechu <mail@grze.ch>
2017-10-14 23:15:12 +0200

    deprecated component injectors removed

--- frogment/src/main/java/ch/grze/frogment/core/component/AbstractActivityComponentInjector.kt
+++
@@ -1,9 +0,0 @@
-package ch.grze.frogment.core.component
-
-import android.app.Activity
-
-@Deprecated("fallback, use ComponentInjector")
-abstract class AbstractActivityComponentInjector : ComponentInjector<Activity> {
-    @Throws(ClassCastException::class)
-    protected fun <T> getTypedActivity(activity: Activity): T = activity as T
-}
--- frogment/src/main/java/ch/grze/frogment/core/component/AbstractFragmentComponentInjector.kt
+++
@@ -1,9 +0,0 @@
-package ch.grze.frogment.core.component
-
-import android.support.v4.app.Fragment
-
-@Deprecated("fallback, use ComponentInjector")
-abstract class AbstractFragmentComponentInjector : ComponentInjector<Fragment> {
-    @Throws(ClassCastException::class)
-    protected fun <T> getTypedFragment(fragment: Fragment): T = fragment as T
-}
